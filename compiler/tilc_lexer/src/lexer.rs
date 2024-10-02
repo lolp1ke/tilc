2dd6711d07c0dab58886e60f6c76288f5ca580d5
Alibek <freed.alibek@gmail.com>
2024-10-02 14:16:51 +0800

    new lexer methods and fixes of basic methods (peek, nth)

--- compiler/tilc_lexer/src/lexer.rs
+++ compiler/tilc_lexer/src/lexer.rs
@@ -6,7 +6,32 @@ use crate::token::{
 };
 
 
-const EOF: char = '\0';
+const EOF_CHAR: char = '\0';
+const RAW_CHAR: char = 's';
+
+
+fn is_whitespace(char: char) -> bool {
+  return match char {
+    ' ' | '\t' | '\r' | '\n' => true,
+
+    _ => false,
+  };
+}
+fn is_identifier_start(char: char) -> bool {
+  return match char {
+    c if c.is_ascii_alphabetic() || c == '_' => true,
+
+    _ => false,
+  };
+}
+fn is_identifier(char: char) -> bool {
+  return match char {
+    c if c.is_ascii_alphanumeric() || c == '_' => true,
+
+    _ => false,
+  };
+}
+
 
 pub struct Lexer<'a> {
   chars: Chars<'a>,
@@ -22,25 +47,51 @@ impl<'a> Lexer<'a> {
 
 
   fn peek(&self) -> char {
-    return match self.chars.clone().peekable().peek() {
-      Some(char) => *char,
-      None => EOF,
+    return match self.chars.clone().next() {
+      Some(char) => char,
+      None => EOF_CHAR,
     };
   }
-  fn nth(&mut self, n: u32) -> char {
+  fn nth(&self, n: u32) -> char {
     debug_assert_ne!(n, 0);
-    // let n: u32 = n + 1;
+    let mut chars: Chars<'_> = self.chars.clone();
+
     for _ in 0..n - 1 {
-      self.chars.next();
+      chars.next();
     }
 
-    return self.step().unwrap_or(EOF);
+    return chars.next().unwrap_or(EOF_CHAR);
   }
-  fn step(&mut self) -> Option<char> {
-    return self.chars.next();
+  fn step(&mut self) -> char {
+    return self.chars.next().unwrap_or(EOF_CHAR);
+  }
+  fn consume(&mut self, mut until: impl FnMut(char) -> bool) {
+    while until(self.peek()) {
+      self.step();
+    }
+  }
+  fn current_token_len(&self) -> usize {
+    return self.len_left - self.chars.as_str().len();
+  }
+  fn reset_len(&mut self) {
+    self.len_left = self.chars.as_str().len();
   }
 
 
+  fn whitespace(&mut self) -> TokenKind {
+    self.consume(is_whitespace);
+    return TokenKind::Whitespace;
+  }
+  /// Assumes that previous char satisfies identifier start char
+  fn identifier(&mut self) -> TokenKind {
+    self.consume(is_identifier);
+
+    return TokenKind::Identifier;
+  }
+  /// Assumes that previous char satisfies identifier start char
+  fn raw_identifier(&mut self) -> TokenKind {
+    todo!();
+  }
   fn is_digit(&mut self) -> bool {
     let mut containt_digits: bool = false;
     loop {
@@ -60,12 +111,6 @@ impl<'a> Lexer<'a> {
     }
 
     return containt_digits;
-  }
-  fn is_whitespace(&mut self) -> bool {
-    return match self.chars.next() {
-      Some(char) => char.is_whitespace(),
-      None => true,
-    };
   }
   fn number_kind(&mut self, current_char: char) -> LiteralKind {
     let mut base: Base = Base::Decimal;
@@ -100,6 +145,10 @@ impl<'a> Lexer<'a> {
 
     // TODO: Handle flaoting point number
     return match self.peek() {
+      '.' if self.nth(2) != '.' => {
+        todo!()
+      }
+
       _ => LiteralKind::Int {
         base: Base::Decimal,
       },
@@ -119,12 +168,13 @@ impl<'a> Lexer<'a> {
 
   fn char_to_token(&mut self) -> Token {
     let current_char: char = match self.step() {
-      Some(char) => char,
-      None => return Token::new(Eof, 0),
+      '\0' => return Token::new(Eof, 0),
+      char => char,
     };
 
+
     let token_kind: TokenKind = match current_char {
-      char if self.is_whitespace() => TokenKind::Whitespace,
+      char if is_whitespace(char) => self.whitespace(),
 
       '/' => match self.peek() {
         '/' => self.line_comment(),
@@ -133,10 +183,22 @@ impl<'a> Lexer<'a> {
         _ => TokenKind::Slash,
       },
 
+      RAW_CHAR => match (self.peek(), self.nth(2)) {
+        ('#', char2) if is_identifier_start(char2) => self.raw_identifier(),
+        ('#', _) => todo!(),
+
+        _ => self.identifier(),
+      },
+
+      char if is_identifier_start(char) => self.identifier(),
+
       char @ '0'..='9' => {
         let kind: LiteralKind = self.number_kind(char);
+        let suffix_pos: u32 = self.current_token_len() as u32;
+        // Consumes suffix if applicable
+        self.consume(is_identifier);
 
-        todo!();
+        TokenKind::Literal { kind, suffix_pos }
       }
 
       '\'' => self.char_literal_or_lifetime(),
@@ -169,22 +231,25 @@ impl<'a> Lexer<'a> {
       _ => Unknown,
     };
 
-    todo!()
+    let token: Token = Token::new(token_kind, self.current_token_len());
+    self.reset_len();
+    return token;
   }
 }
 
 
 #[cfg(test)]
 mod test {
-  use crate::token::{Base, LiteralKind};
-
-  use super::Lexer;
+  use crate::{
+    lexer::{Lexer, EOF_CHAR},
+    token::{Base, LiteralKind, Token, TokenKind},
+  };
 
 
   #[test]
   fn peek() {
     assert_eq!(Lexer::new("a").peek(), 'a');
-    assert_eq!(Lexer::new("1").peek(), '1');
+    assert_eq!(Lexer::new("0").peek(), '0');
     assert_eq!(Lexer::new("?").peek(), '?');
     assert_eq!(Lexer::new("\\").peek(), '\\');
     assert_eq!(Lexer::new("").peek(), '\0');
@@ -195,41 +260,63 @@ mod test {
 
 
     assert_eq!(l.nth(2), 'x');
-    assert_eq!(l.nth(3), 'a');
-    assert_eq!(l.nth(4), '(');
-    assert_eq!(l.nth(5), '\0');
+    assert_eq!(l.nth(3), ' ');
+    assert_eq!(l.nth(4), 'b');
+    assert_eq!(l.nth(11), '\0');
   }
   #[test]
   fn step() {
     let mut l: Lexer<'_> = Lexer::new("fx basty()");
 
 
-    assert_eq!(l.step(), Some('f'));
-    assert_eq!(l.step(), Some('x'));
-    assert_eq!(l.step(), Some(' '));
-    assert_eq!(l.step(), Some('b'));
-    assert_eq!(l.step(), Some('a'));
-    assert_eq!(l.step(), Some('s'));
-    assert_eq!(l.step(), Some('t'));
-    assert_eq!(l.step(), Some('y'));
-    assert_eq!(l.step(), Some('('));
-    assert_eq!(l.step(), Some(')'));
-    assert_eq!(l.step(), None);
+    assert_eq!(l.step(), 'f');
+    assert_eq!(l.step(), 'x');
+    assert_eq!(l.step(), ' ');
+    assert_eq!(l.step(), 'b');
+    assert_eq!(l.step(), 'a');
+    assert_eq!(l.step(), 's');
+    assert_eq!(l.step(), 't');
+    assert_eq!(l.step(), 'y');
+    assert_eq!(l.step(), '(');
+    assert_eq!(l.step(), ')');
+    assert_eq!(l.step(), EOF_CHAR);
   }
 
-  // #[test]
-  // fn is_digit() {
-  //   let mut l: Lexer<'_> = Lexer::new("1");
 
-  //   assert_eq!(
-  //     l.number_kind(l.peek()),
-  //     LiteralKind::Int {
-  //       base: Base::Decimal
-  //     }
-  //   );
-  //   assert_eq!(Lexer::new("1_000").is_digit(), true);
-  //   assert_eq!(Lexer::new("0e101").is_digit(), true);
-  //   assert_eq!(Lexer::new("0s71").is_digit(), true);
-  //   assert_eq!(Lexer::new("0o1A1").is_digit(), true);
-  // }
+  #[test]
+  fn char_to_token() {
+    let mut l: Lexer<'_> = Lexer::new("fx basty() -> i32 {qaitar 0_i32;}");
+
+
+    assert_eq!(l.char_to_token(), Token::new(TokenKind::Identifier, 2)); // fx
+    assert_eq!(l.char_to_token(), Token::new(TokenKind::Whitespace, 1)); // ' '
+    assert_eq!(l.char_to_token(), Token::new(TokenKind::Identifier, 5)); // basty
+    assert_eq!(l.char_to_token(), Token::new(TokenKind::OpenParen, 1)); // (
+    assert_eq!(l.char_to_token(), Token::new(TokenKind::CloseParen, 1)); // )
+    assert_eq!(l.char_to_token(), Token::new(TokenKind::Whitespace, 1)); // ' '
+    assert_eq!(l.char_to_token(), Token::new(TokenKind::Minus, 1)); // -
+    assert_eq!(l.char_to_token(), Token::new(TokenKind::Gt, 1)); // >
+    assert_eq!(l.char_to_token(), Token::new(TokenKind::Whitespace, 1)); // ' '
+    assert_eq!(l.char_to_token(), Token::new(TokenKind::Identifier, 3)); // i32
+    assert_eq!(l.char_to_token(), Token::new(TokenKind::Whitespace, 1)); // ' '
+    assert_eq!(l.char_to_token(), Token::new(TokenKind::OpenBrace, 1)); // {
+    assert_eq!(l.char_to_token(), Token::new(TokenKind::Identifier, 6)); // qaitar
+    assert_eq!(l.char_to_token(), Token::new(TokenKind::Whitespace, 1)); // ' '
+    assert_eq!(
+      l.char_to_token(),
+      Token::new(
+        TokenKind::Literal {
+          kind: LiteralKind::Int {
+            base: Base::Decimal
+          },
+          suffix_pos: 2
+        },
+        5
+      )
+    ); // 0_i32
+
+    assert_eq!(l.char_to_token(), Token::new(TokenKind::Semicolon, 1)); // ;
+    assert_eq!(l.char_to_token(), Token::new(TokenKind::CloseBrace, 1)); // }
+    assert_eq!(l.char_to_token(), Token::new(TokenKind::Eof, 0)); // '\0'
+  }
 }
